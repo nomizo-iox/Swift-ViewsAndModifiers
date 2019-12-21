95c6a0e651e8f4dd97bcabec517fd94409921b1c
nomizo-iox <nomizo.iox@gmail.com>
2019-12-21 08:32:56 -0600

    adding the initial infomraiton.

---
+++ ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Samuel F. Ademola on 12/21/19.
+//  Copyright © 2019 Nomizo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .background(Color.red)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
