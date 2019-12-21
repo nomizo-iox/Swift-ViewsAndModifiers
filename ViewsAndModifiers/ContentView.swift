cc41ba23464380e81e47f586b75f271452b76661
nomizo-iox <nomizo.iox@gmail.com>
2019-12-21 12:19:33 -0600

    Adding Extensions and Custome Modifiers.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,27 +8,55 @@
 
 import SwiftUI
 
-struct CapsuleText: View {
+
+// Creating custome modifier
+struct Title: ViewModifier {
+    func body(content: Content) -> some View{
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+struct Watermark: ViewModifier {
     var text: String
     
-    var body: some View {
-        Text(text)
-            .font(.largeTitle)
-            .padding()
-            .foregroundColor(.white)
-            .background(Color .blue)
-            .clipShape(Capsule())
+    func body(content: Content) -> some View {
+        ZStack (alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+// Extends the ability to use the Title struct as a modifier
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+    
+    func watermarkStyle(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
     }
 }
 
 
+
 struct ContentView: View {
     var body: some View {
-        VStack (spacing: 10) {
-            CapsuleText(text: "First")
-                .foregroundColor(.white)
-            CapsuleText(text: "Second")
-                .foregroundColor(.yellow)
+        VStack {
+            Text("Hello World")
+                .titleStyle()
+            
+            Text("Howdy")
+                .frame(width: 300, height: 200)
+            .watermarkStyle(with: "Hacking With SWift")
         }
     }
 }
