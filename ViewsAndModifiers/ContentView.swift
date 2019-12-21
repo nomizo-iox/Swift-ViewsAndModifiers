9a6c104c9b2579944c291f184416cf51a4926435
nomizo-iox <nomizo.iox@gmail.com>
2019-12-21 16:09:22 -0600

    adding the finished product.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,41 +8,28 @@
 
 import SwiftUI
 
-
-// Creating custome modifier
-struct Title: ViewModifier {
-    func body(content: Content) -> some View{
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(Color.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
+// Creating custome container
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
     
-    func body(content: Content) -> some View {
-        ZStack (alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
+    var body: some View {
+        VStack {
+            ForEach ( 0 ..< rows) { row in
+                HStack {
+                    ForEach( 0 ..< self.columns) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
         }
     }
-}
-// Extends the ability to use the Title struct as a modifier
-extension View {
-    func titleStyle() -> some View {
-        self.modifier(Title())
-    }
     
-    func watermarkStyle(with text: String) -> some View {
-        self.modifier(Watermark(text: text))
+    init (rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
     }
 }
 
@@ -50,16 +37,16 @@ extension View {
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello World")
-                .titleStyle()
+        GridStack(rows: 4, columns: 4) { row, col in
             
-            Text("Howdy")
-                .frame(width: 300, height: 200)
-            .watermarkStyle(with: "Hacking With SWift")
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+            
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
